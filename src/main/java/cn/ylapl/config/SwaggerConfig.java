86853664068cac745eb5d84adbd87acdc02e4d62
杨乐 <yangle@xueleyun.com>
2017-11-27 16:29:47 +0800

    更改springboot版本到1.5.8 更新swaggerconfig 更新timeInterceptor 更新pom

--- src/main/java/cn/ylapl/config/SwaggerConfig.java
+++ src/main/java/cn/ylapl/config/SwaggerConfig.java
@@ -6,9 +6,12 @@ import org.springframework.web.context.request.async.DeferredResult;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 /**
  * @author yangle
@@ -28,19 +31,21 @@ public class SwaggerConfig {
                 .pathMapping("/")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("cn.ylapl.controller"))
-                .paths(PathSelectors.any())//过滤的接口
+                .paths(PathSelectors.any())
                 .build().apiInfo(demoApiInfo());
     }
 
     private ApiInfo demoApiInfo() {
-
-        return new ApiInfo("豆神科技",//大标题
-                "豆神科技",//小标题
+        Contact contact = new Contact("Angle", "http://blog.ylapl.cn", "1024920977@qq.com");
+        return new ApiInfo(
+                "",//大标题
+                "",//小标题
                 "1.0",//版本
-                "NO terms of service",
-                "Angle",//作者
+                "逗神科技",
+                contact,//作者
                 "The Apache License, Version 2.0",//链接显示文字
-                "http://www.apache.org/licenses/LICENSE-2.0.html"//网站链接
+                "http://www.apache.org/licenses/LICENSE-2.0.html",//网站链接
+                Collections.emptyList()
         );
     }
 }
--- src/main/java/cn/ylapl/util/TimeInterceptor.java
+++ src/main/java/cn/ylapl/util/TimeInterceptor.java
@@ -1,6 +1,5 @@
-package cn.ylapl.util.timeInterceptor;
+package cn.ylapl.util;
 
-import cn.ylapl.util.JacksonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -27,7 +26,7 @@ public class TimeInterceptor {
     /**
      * service层的统计耗时切面，类型必须为final String类型的,注解里要使用的变量只能是静态常量类型的
      */
-    private static final String POINT = "execution (* cn.ylapl.service.*.*(..))";
+    private static final String POINT = "execution (* cn.ylapl..*(..))";
 
     /**
      * 统计方法执行耗时Around环绕通知
@@ -35,23 +34,23 @@ public class TimeInterceptor {
      * @return              方法执行返回值
      */
     @Around(POINT)
-    public Object timeAround(ProceedingJoinPoint joinPoint) {
+    public Object timeAround(ProceedingJoinPoint joinPoint) throws Throwable {
         // 定义返回对象、得到方法需要的参数
-        Object obj = null;
+        Object obj;
         Object[] args = joinPoint.getArgs();
         long startTime = System.nanoTime();
-
-        try {
-            obj = joinPoint.proceed(args);
-        } catch (Throwable e) {
-            log.error("方法参数：{}", JacksonUtils.console(args));
-            log.error("统计某方法执行耗时环绕通知出错", e);
-        }
 
         // 获取执行的方法名
         long endTime = System.nanoTime();
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         String methodName = signature.getDeclaringTypeName() + "." + signature.getName();
+
+        try {
+            obj = joinPoint.proceed(args);
+        } catch (Throwable e) {
+            log.error("方法名:{}, 方法参数：{}", methodName, JacksonUtils.objectToJson(args));
+            throw e;
+        }
 
         // 打印耗时的信息
         this.printExecTime(methodName, startTime, endTime);
@@ -68,7 +67,7 @@ public class TimeInterceptor {
     private void printExecTime(String methodName, long startTime, long endTime) {
         long diffTime = endTime - startTime;
         if (diffTime > ONE_MINUTE) {
-            log.info("-----" + methodName + " 方法执行耗时：" + diffTime + " ms");
+            log.info("-----" + methodName + " 方法执行耗时：" + diffTime + " ns");
         }
     }
 }
